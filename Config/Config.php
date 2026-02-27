80d350f1587572911792fd78df5e133f6ab34ccd
Mace98 <98475244+Mace98@users.noreply.github.com>
2026-02-27 12:19:41 -0500

    Clear sensitive configuration values
    Removed sensitive database and base URL information.

--- Config/Config.php
+++ Config/Config.php
@@ -1,15 +1,15 @@
 <?php 
 	
-	const BASE_URL = "http://localhost/demos/proyect_preoperacional/";
+	const BASE_URL = "";
 	const LIBS = "Libraries/";
 	const VIEWS = "Views/";
 
 	date_default_timezone_set("America/Bogota");
 
-	const DB_HOST = "192.168.0.101";
-	const DB_USER = "wilson";
-	const DB_PASS = "wilson";
-	const DB_NAME = "geofinderc";
+	const DB_HOST = "";
+	const DB_USER = "";
+	const DB_PASS = "";
+	const DB_NAME = "";
 	const DB_CHAR = "client_encoding=utf8";
 
 	const SPD = ".";
